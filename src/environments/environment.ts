7904a119a1e8cc49f0299eaae7e5f1c50d3f8034
alexandre queiroz <alexandre.queiroz@syngenta.com>
2025-07-03 08:38:46 -0300

    .gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  defaultauth: 'fackbackend',
+  firebaseConfig: {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
+  }
+};
