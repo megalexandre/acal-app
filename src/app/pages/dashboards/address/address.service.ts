c4c13ecc13ed7899e7bf7578c70bf578e0347b81
alexandre queiroz <alexandre.queiroz@syngenta.com>
2025-07-03 11:08:17 -0300

    change env import

--- src/app/pages/dashboards/address/address.service.ts
+++ src/app/pages/dashboards/address/address.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Address } from './address.model';
-
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class AddressService {
