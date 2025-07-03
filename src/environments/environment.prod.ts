c6e1bf5763deff612aa4678e3ed4eef028a19b58
alexandre queiroz <alexandre.queiroz@syngenta.com>
2025-07-03 20:02:14 -0300

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  apiUrl: 'http://w4sow44skk08o8kgcok0cw00.85.31.230.47.sslip.io',
+  apiUrl: 'https://acalapp.shop',
 
   defaultauth: 'fackbackend',
   firebaseConfig: {
